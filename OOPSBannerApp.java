fd92e49eff5a24d68652c6695a2251a4b9b1e04d
md8766 <md8766@srmist.edu.in>
2026-02-16 13:38:33 +0530

    UC2: Add OOPS banner output

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,18 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
+
+        // UC1: simple text
         System.out.println("OOPS");
+        System.out.println();
+
+        // UC2: 7-line banner using * and spaces (no loops)
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println(" *****   *****   *****  *     *");
+        System.out.println("*       *     *       * *     *");
+        System.out.println("*       *     *       * *     *");
+        System.out.println("*       *     * *****   ***** ");
     }
 }
